0d121369a16b9866cb81c8b4f0f8d2bd7a3f06fc
David Chua <zhchua@gmail.com>
2014-12-13 11:02:50 +0800

    remove new_resource

--- custom_rake/recipes/default.rb
+++ custom_rake/recipes/default.rb
@@ -1,7 +1,5 @@
-rails_env = new_resource.environment["RAILS_ENV"]
-
 execute 'run_rake' do
   cwd release_path
   command "bundle exec rake #{node[:rake][:command]}"
-  environment "RAILS_ENV" => rails_env
+  environment "RAILS_ENV" => "production"
 end
