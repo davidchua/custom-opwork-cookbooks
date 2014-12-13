6a357455e339c5ccc95e8d8b47023ed180bc8be1
David Chua <zhchua@gmail.com>
2014-12-13 11:19:01 +0800

    Modify path

--- custom_rake/recipes/default.rb
+++ custom_rake/recipes/default.rb
@@ -1,5 +1,5 @@
 execute 'run_rake' do
-  cwd node[:deploy][node[:app_short_name]][:deploy_to]
+  cwd node[:deploy][node[:app_short_name]][:deploy_to] + "/current"
   command "bundle exec rake #{node[:rake][:command]}"
   environment "RAILS_ENV" => "production"
 end
