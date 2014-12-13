fdce2f5a7c493363a8da072604ca5366878d6c0a
David Chua <zhchua@gmail.com>
2014-12-13 11:12:44 +0800

    Add shortname

--- custom_rake/recipes/default.rb
+++ custom_rake/recipes/default.rb
@@ -1,5 +1,5 @@
 execute 'run_rake' do
-  cwd release_path
+  cwd node[:deploy][node[:app_short_name]][:deploy_to]
   command "bundle exec rake #{node[:rake][:command]}"
   environment "RAILS_ENV" => "production"
 end
